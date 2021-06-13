ddaa0d02ce6d0ff8ea05ffd5e1c6f449898c3a32
Vaibhav Saran <vaibhavsaran8@gmail.com>
2021-06-13 22:58:51 +0530

    New Questions and Code beautification
    Added New Questions 
    Rectified Q3 and Q5 for their language was a bit misleading
    Beautified cp12.cpp

--- cp12.cpp
+++ cp12.cpp
@@ -1,7 +1,13 @@
+/*Problem Statement: Given two integer,L and R,find the maximal value of a xor b,written a+b,
+where a and b satisfy the follwoing condition:
+            L<= a <= b <=R
+Example:- if L=11 and R=12 then
+    11 + 11 = 0 ['+' represnt xor operation]
+    11 + 12 = 7
+    12 + 12 = 0
+Result:- 7*/
 #include <bits/stdc++.h>
-
 using namespace std;
-
 int main()
 {
     int l,r,maxA,maxB,temp,best;
